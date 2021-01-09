3da68b00a4f032f961d4e19d35079b51c6ef4c56
GiovaneNalin <56086259+GiovaneNalin@users.noreply.github.com>
2021-01-09 13:52:07 -0300

    Add files via upload

--- alteracao_telefone.php
+++ alteracao_telefone.php
@@ -2,7 +2,7 @@
 	$id = filter_input(INPUT_GET, "id");
 	$telefone = filter_input(INPUT_GET, "telefone");
 	
-	$link = mysqli_connect("localhost", "root", "", "tcc");
+	$link = mysqli_connect("db4free.net:3306", "giovane_nalin", "giovanenalin", "carteirinha");
 	if($link){
 		echo $id;
 
